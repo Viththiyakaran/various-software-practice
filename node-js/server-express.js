9cd8a8819da2cd5c2718e2d045fb0291433c30ea
Viththiyakaran <battikaran6@gmail.com>
2020-04-14 11:50:56 +0530

    Install express and create routes

--- node-js/server-express.js
+++ node-js/server-express.js
@@ -8,4 +8,14 @@ app.get('/', function(req, res ){
         res.send("Hello ! express server");
 });
 
+
+app.get('/about', function(req , res){
+     res.send("This is about page");
+});
+
+
+app.get('/contact', function(req , res){
+  res.send("This is contact page");
+});
+
 app.listen(3000);
