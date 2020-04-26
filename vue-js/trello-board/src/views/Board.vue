26a243e5081245bcd3c3f46be8cac01eb8cff454
Viththiyakaran <battikaran6@gmail.com>
2020-04-27 00:23:27 +0530

    Update trello files

--- vue-js/trello-board/src/views/Board.vue
+++ vue-js/trello-board/src/views/Board.vue
@@ -1,25 +1,68 @@
 <template>
   <div class="board">
+    <div class="flex flex-row items-start">
+      <BoardColumn
+        v-for="(column, $columnIndex) of board.columns"
+        :key="$columnIndex"
+        :column="column"
+        :columnIndex="$columnIndex"
+        :board="board"
+      />
 
+      <div class="column flex">
+        <input
+          type="text"
+          class="p-2 mr-2 flex-grow"
+          placeholder="New Column Name"
+          v-model="newColumnName"
+          @keyup.enter="createColumn"
+        >
+      </div>
+    </div>
+
+    <div
+      class="task-bg"
+      v-if="isTaskOpen"
+      @click.self="close"
+    >
+      <router-view/>
+    </div>
   </div>
 </template>
 
 <script>
-export default {
+import { mapState } from 'vuex'
+import BoardColumn from '@/components/BoardColumn'
 
+export default {
+  components: { BoardColumn },
+  data () {
+    return {
+      newColumnName: ''
+    }
+  },
+  computed: {
+    ...mapState(['board']),
+    isTaskOpen () {
+      return this.$route.name === 'task'
+    }
+  },
+  methods: {
+    close () {
+      this.$router.push({ name: 'board' })
+    },
+    createColumn () {
+      this.$store.commit('CREATE_COLUMN', {
+        name: this.newColumnName
+      })
+
+      this.newColumnName = ''
+    }
+  }
 }
 </script>
 
 <style lang="css">
-.task {
-  @apply flex items-center flex-wrap shadow mb-2 py-2 px-2 rounded bg-white text-grey-darkest no-underline;
-}
-
-.column {
-  @apply bg-grey-light p-2 mr-4 text-left shadow rounded;
-  min-width: 350px;
-}
-
 .board {
   @apply p-4 bg-teal-dark h-full overflow-auto;
 }
--- vue-js/trello-board/src/views/Task.vue
+++ vue-js/trello-board/src/views/Task.vue
@@ -1,11 +1,42 @@
 <template>
   <div class="task-view">
+    <div class="flex flex-col flex-grow items-start justify-between px-4">
+      <input
+        type="text"
+        class="p-2 w-full mr-2 block text-xl font-bold"
+        :value="task.name"
+        @change="updateTaskProperty($event, 'name')"
+        @keyup.enter="updateTaskProperty($event, 'name')"
+      >
 
+      <textarea
+        class="relative w-full bg-transparent px-2 border mt-2 h-64 border-none leading-normal"
+        :value="task.description"
+        @change="updateTaskProperty($event, 'description')"
+      />
+    </div>
   </div>
 </template>
 
 <script>
+import { mapGetters } from 'vuex'
+
 export default {
+  computed: {
+    ...mapGetters(['getTask']),
+    task () {
+      return this.getTask(this.$route.params.id)
+    }
+  },
+  methods: {
+    updateTaskProperty (e, key) {
+      this.$store.commit('UPDATE_TASK', {
+        task: this.task,
+        key,
+        value: e.target.value
+      })
+    }
+  }
 }
 </script>
 
