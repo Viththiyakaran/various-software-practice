3b15bfb745b1d3228523c92dd45aaf1c85d753a0
Viththiyakaran <battikaran6@gmail.com>
2020-05-08 01:24:45 +0530

    Updates

---
+++ precache-manifest.7da1d92225332840cb49cf5e3337c33e.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9cc8f47462ddf1fa0a60b103bde107fe",
+    "url": "/weather-finder-app/index.html"
+  },
+  {
+    "revision": "6942a629f636c1a21cc8",
+    "url": "/weather-finder-app/static/css/main.57d2366f.chunk.css"
+  },
+  {
+    "revision": "3eb0fbf182770295a78b",
+    "url": "/weather-finder-app/static/js/2.af9b9637.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/weather-finder-app/static/js/2.af9b9637.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6942a629f636c1a21cc8",
+    "url": "/weather-finder-app/static/js/main.e084b476.chunk.js"
+  },
+  {
+    "revision": "6cbe0173528a5cd3d38c",
+    "url": "/weather-finder-app/static/js/runtime-main.2d2d562b.js"
+  },
+  {
+    "revision": "c97cb84b74a52edcb496295687e87822",
+    "url": "/weather-finder-app/static/media/cold-bg.c97cb84b.jpg"
+  },
+  {
+    "revision": "635ed87d2cc826fb213057ac40dfaff1",
+    "url": "/weather-finder-app/static/media/warm-bg.635ed87d.jpg"
+  }
+]);
