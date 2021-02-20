2b0d2ad27f2e17a9006b76d87e202bf3a72c9da7
Viththiyakaran <battikaran6@gmail.com>
2021-02-20 15:32:53 +0800

    Add list components and show on index

--- react-js/birthday_remaider/src/App.js
+++ react-js/birthday_remaider/src/App.js
@@ -1,8 +1,16 @@
 import React, { useState } from 'react';
-//import data from './data';
-//import List from './List';
+import data from './data';
+import List from './List';
 function App() {
-  return <h2>reminder project setup</h2>;
+  const [people, setPeople] = useState(data)
+  
+  return <main>
+    <section className='container'> 
+       <h3> {people.length} Birth days today  </h3>
+       <List people={ people}/>
+       <button onClick={ ()=> console.log("Raj")} > Clear ALL </button>
+    </section>
+  </main>;
 }
 
 export default App;
