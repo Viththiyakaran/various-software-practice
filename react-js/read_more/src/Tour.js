0e38dc1901452dfb971c36970c05e5faba32649b
Viththiyakaran <battikaran6@gmail.com>
2021-02-20 17:05:03 +0800

    Add readmore and showless funcation

--- react-js/read_more/src/Tour.js
+++ react-js/read_more/src/Tour.js
@@ -1,7 +1,7 @@
 import React, { useState } from 'react';
 
 const Tour = ({id,image,info,price,name}) => {
- 
+ const [ readmore, setReadmore] = useState(false);
   return <article className='single-tour'>
     <img src={image} alt={name} />
     <footer>
@@ -9,7 +9,8 @@ const Tour = ({id,image,info,price,name}) => {
           <h4> {name}</h4>
           <h4 className='tour-price'>{ price}</h4> 
         </div>
-        <p>{info} </p>
+        <p>{ readmore?info: `${info.substring(0,200)}...`} </p>
+        <button onClick={()=> setReadmore(!readmore)}>{readmore?'show less':'ReadMore'} </button>
         <button className='delete-btn'> Not Need  </button>
       
     </footer>
